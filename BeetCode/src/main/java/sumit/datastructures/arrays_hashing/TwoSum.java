87245e4821397337e5550a5d8d66636152fd9f31
“sumit8669” <sumit2628r@gmail.com>
2025-02-20 00:59:59 +0530

    Two Sum HM

--- BeetCode/src/main/java/sumit/datastructures/arrays_hashing/TwoSum.java
+++ BeetCode/src/main/java/sumit/datastructures/arrays_hashing/TwoSum.java
@@ -1,5 +1,8 @@
 package sumit.datastructures.arrays_hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length; i++){
@@ -10,5 +13,19 @@ public class TwoSum {
             }
         }
         return new int[] {};
+    }
+    public int[] twoSumHM(int[] nums, int target) {
+        Map<Integer,Integer> sum = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int num = nums[i];
+            int diff = target -num;
+
+            if(sum.containsKey(diff)){
+                return new int [] {sum.get(diff),i};
+            }
+            sum.put(num,i);
+        }
+        return new int [] {};
     }
 }
