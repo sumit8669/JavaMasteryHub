8bfb6f3d25635e95ea16ac14d2e80db2e09a8604
“sumit8669” <sumit2628r@gmail.com>
2025-02-24 10:31:14 +0530

    solid principle update

--- BeetCode/src/main/java/sumit/designpattern/SOLID/Main.java
+++ BeetCode/src/main/java/sumit/designpattern/SOLID/Main.java
@@ -1,7 +1,15 @@
 package sumit.designpattern.SOLID;
 
+import sumit.designpattern.SOLID.payment.services.BankTransfer;
+import sumit.designpattern.SOLID.payment.services.CreditCard;
+import sumit.designpattern.SOLID.processor.PaymentProcessor;
+
 public class Main {
     public static void main(String[] args) {
+        PaymentProcessor bt = new PaymentProcessor(new BankTransfer());
+        bt.processPayment();
 
+        PaymentProcessor cr = new PaymentProcessor(new CreditCard());
+        cr.processPayment();
     }
 }
--- BeetCode/src/main/java/sumit/designpattern/SOLID/payment/services/CreditCard.java
+++ BeetCode/src/main/java/sumit/designpattern/SOLID/payment/services/CreditCard.java
@@ -2,9 +2,10 @@ package sumit.designpattern.SOLID.payment.services;
 
 import sumit.designpattern.SOLID.payment.interfaces.Payable;
 import sumit.designpattern.SOLID.payment.interfaces.Refundable;
+import sumit.designpattern.SOLID.payment.model.Payment;
 
 //Each class follows SRP (one responsibility) and LSP (can be substituted safely).
-public class CreditCard implements Payable, Refundable {
+public class CreditCard implements Payable, Refundable , Payment {
 
     @Override
     public void pay() {
